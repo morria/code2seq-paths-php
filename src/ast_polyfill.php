51ce983de980a0593dda37a718c05a227ebeab42
Andrew S. Morrison <morria@users.noreply.github.com>
2023-03-29 15:09:41 +0000

    Support PHP 7

--- src/ast_polyfill.php
+++ src/ast_polyfill.php
@@ -11,6 +11,9 @@ foreach ([
     'ast\\AST_TYPE_INTERSECTION' => 145,
     'ast\\AST_ENUM_CASE' => 1026,
     'ast\\AST_CALLABLE_CONVERT' => 3,
+    'ast\\flags\\TYPE_TRUE' => 3,
+    'ast\\flags\\TYPE_FALSE' => 2,
+    'ast\\flags\\TYPE_NEVER' => 17,
 ] as $const => $value) {
     if (!\defined($const)) {
         define($const, 145);
